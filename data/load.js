90512daa061e7b384b98e625832d144b13811120
Karel Renaldi <karelrenaldi8@gmail.com>
2021-04-26 16:55:27 +0700

    implement task api and refactor task model

--- data/load.js
+++ data/load.js
@@ -1,50 +1,41 @@
-/*eslint-disable*/
-const fs = require("fs");
-const User = require("../src/models/User");
-const Task = require("../src/models/Task");
-// const Chat = require("../src/models/Chat");
+const fs = require('fs');
+const { Task } = require('../src/models/Task');
 
-const dotenv = require("dotenv");
+const dotenv = require('dotenv');
 
-dotenv.config({ path: "../../config.env" });
+dotenv.config({ path: '../../config.env' });
 
-const mongoose = require("mongoose");
-const db = process.env.ATLAS_URL;
+const mongoose = require('mongoose');
+const db = 'mongodb+srv://stima2:stima@cluster0.hbohv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 (async () => {
-  await mongoose.connect(db, {
-    // options for deal some deprecations warning when creating our app
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
-  });
-  console.log("Database connection successful");
+    await mongoose.connect(db, {
+        // options for deal some deprecations warning when creating our app
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true,
+    });
+    console.log('Database connection successful');
 })();
 
-// const chat = JSON.parse(fs.readFileSync(`${__dirname}/chat.json`, "utf-8"));
-const users = JSON.parse(fs.readFileSync(`${__dirname}/user.json`, "utf-8"));
-const tasks = JSON.parse(fs.readFileSync(`${__dirname}/task.json`, "utf-8"));
+const tasks = JSON.parse(fs.readFileSync(`${__dirname}/task.json`, 'utf-8'));
 
 const updateItem = async () => {
-  await User.create(users);
-  await Task.create(tasks);
-  // await Chat.create(chat);
+    await Task.create(tasks);
 
-  console.log("Update Item");
-  process.exit();
+    console.log('Update Item');
+    process.exit();
 };
 
 const deleteItem = async () => {
-  await User.deleteMany();
-  await Task.deleteMany();
-  // await Chat.deleteMany();
+    await Task.deleteMany();
 
-  console.log("Delete Items");
-  process.exit();
+    console.log('Delete Items');
+    process.exit();
 };
 
-if (process.argv.includes("--update")) {
-  updateItem();
-} else if (process.argv.includes("--delete")) {
-  deleteItem();
+if (process.argv.includes('--update')) {
+    updateItem();
+} else if (process.argv.includes('--delete')) {
+    deleteItem();
 }
