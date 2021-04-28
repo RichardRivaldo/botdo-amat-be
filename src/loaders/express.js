d39aa1c0a4b9628c9b54003b30c284a02e64361f
pege <mpg066@gmail.com>
2021-04-28 14:01:33 +0700

    benerin indentasi

--- src/loaders/express.js
+++ src/loaders/express.js
@@ -6,44 +6,45 @@ import { MAX_REQUEST_SIZE } from '../helper/const';
 import { errors as celebrateErrorHandler } from 'celebrate';
 
 export default ({ app }) => {
-                                // It shows the real origin IP in the heroku or Cloudwatch logs
-                                app.enable('trust proxy');
+    // It shows the real origin IP in the heroku or Cloudwatch logs
+    app.enable('trust proxy');
 
-                                // Enable Cross Origin Resource Sharing to all origins by deinult
-                                let corsOptions = {
-                                    credentials: true,
-                                    origin: true,
-                                };
+    // Enable Cross Origin Resource Sharing to all origins by deinult
+    let corsOptions = {
+        credentials: true,
+        // origin : "https://botdo.michaelpege.com",
+        origin: true,
+    };
 
-                                app.use(cors(corsOptions));
+    app.use(cors(corsOptions));
 
-                                // Middleware that transforms the raw string of req.body into json
-                                app.use(bodyParser.json({ limit: MAX_REQUEST_SIZE }));
+    // Middleware that transforms the raw string of req.body into json
+    app.use(bodyParser.json({ limit: MAX_REQUEST_SIZE }));
 
-                                // Health Check endpoints
-                                app.get('/status', (req, res) => {
-                                    res.status(200).end();
-                                });
+    // Health Check endpoints
+    app.get('/status', (req, res) => {
+        res.status(200).end();
+    });
 
-                                // Load API routes << Important
-                                app.use(config.api.prefix, routes());
+    // Load API routes << Important
+    app.use(config.api.prefix, routes());
 
-                                // Catch 404 and forward to error handler
-                                app.use((req, res, next) => {
-                                    const err = new Error('Not Found');
-                                    err['status'] = 404;
-                                    next(err);
-                                });
+    // Catch 404 and forward to error handler
+    app.use((req, res, next) => {
+        const err = new Error('Not Found');
+        err['status'] = 404;
+        next(err);
+    });
 
-                                app.use(celebrateErrorHandler());
+    app.use(celebrateErrorHandler());
 
-                                // Error handler
-                                app.use((err, req, res, next) => {
-                                    res.status(err.status || 500);
-                                    res.json({
-                                        errors: {
-                                            message: err.message,
-                                        },
-                                    });
-                                });
-                            };
+    // Error handler
+    app.use((err, req, res, next) => {
+        res.status(err.status || 500);
+        res.json({
+            errors: {
+                message: err.message,
+            },
+        });
+    });
+};
